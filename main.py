969e6e088d04081b0fdaa539233843ad0531ab85
unknown <alexrugina8@yahoo.com>
2021-10-13 11:35:38 +0300

    Tema_Rezolvata

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def citire_lista():
     list = []
     n = int(input('Dati nr de elemente: '))
     for i in range(n):
-        list.append(int(input('L[' + str(i) + '] =')))
+        list.append(int(input('L[' + str(i) + ']= ')))
     return list
 
 
@@ -22,7 +22,7 @@ def elemente_pare(list):
     return True
 
 
-def test_elemente_pare(list):
+def test_elemente_pare():
     assert elemente_pare([9, 81, 73]) == False
     assert elemente_pare([100, 58, 2, 10]) == True
 
@@ -33,22 +33,22 @@ def get_longest_all_even(list):
     :param list: Reprezinta lista de numere intregi .
     :return: Returneaza cea mai lunga subsecventa in care toate elementele sunt numere pare din list .
     '''
-    test_elemente_pare(list)
+    test_elemente_pare()
     subsecventa_max = []
     for i in range(len(list)):
         for j in range(i, len(list)):
-            if ToateElemSuntPare(list[i:j + 1]) and len(subsecventa_max) < len(list[i:j + 1]):
+            if elemente_pare(list[i:j + 1]) and len(subsecventa_max) < len(list[i:j + 1]):
                 subsecventa_max = list[i:j + 1]
     return subsecventa_max
 
 
 def test_get_longest_all_even():
-    assert get_longest_all_even([5, 7]) == None
+    assert get_longest_all_even([5, 7]) is None
     assert get_longest_all_even([10, 20, 22]) == [10, 20, 22]
     assert get_longest_all_even([15, 12, 14, 55, 1000]) == [12, 14]
 
 
-def medie_lista (list):
+def medie_lista(list):
     '''
     Functia calculeaza media a tuturor elementelor din lista .
     :param list: Lista de nr intregi .
@@ -67,7 +67,7 @@ def test_medie_lista ():
     assert medie_lista([7,6,10,22]) == 11.25
 
 
-def get_longest_average_below (lst , average):
+def get_longest_average_below (list , average):
     '''
     Functia determina cea mai lunga subsecventa in care toate elementele au media mai mica decat o valoare citita .
     :param list: Lista de nr intregi .
@@ -78,16 +78,21 @@ def get_longest_average_below (lst , average):
     subsecventa_max = []
     for i in range(len(list)):
         for j in range(i, len(list)):
-            if mediaElem(list[i:j + 1]) < average and len(subsecventa_max) < len(list[i:j + 1]):
-                subsecventa_max = lst[i:j + 1]
+            if medie_lista(list[i:j + 1]) < average and len(subsecventa_max) < len(list[i:j + 1]):
+                subsecventa_max = list[i:j + 1]
     return subsecventa_max
+
+
+def test_get_longest_average_below ():
+    assert get_longest_average_below([5,7,10,30],9) == [5,7,10]
+    assert get_longest_average_below([50],10) is None
 
 
 def printMenu():
     print ('1. Citire date')
     print ('2. Determinare cea mai lungă subsecvență in care toate numerele sunt pare')
     print ('3. Determinare cea mai lungă subsecvență in care media a numerelor nu depășește o valoare citită')
-
+    print ('4. Iesire')
 
 def main():
     list = []
@@ -99,6 +104,15 @@ def main():
 
         elif optiune == '2':
             print(get_longest_all_even(list))
+
+        elif optiune == '3':
+            average=int(input('Dati valoarea pentru a compara media: '))
+            print(get_longest_average_below((list), average))
+
+        elif optiune == '4':
+            break
+        else:
+            print("Optiune gresita! Reincercati!")
 
 
 main()
